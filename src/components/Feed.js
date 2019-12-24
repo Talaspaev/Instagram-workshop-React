2a29d8a85031a31dc8b0611b51fcd39045398bfc
Mukhamed Talaspaev <7636muha@gmail.com>
2019-12-24 22:31:37 +0300

    feat:header

---
+++ src/components/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Posts from './Posts';
+import User from './User';
+
+export default function Feed() {
+    return (
+        <div className="container feed">
+            <Posts/>
+            <User/>
+        </div>
+    )
+}
