1282d32220977190d0070cd2fe7eab7f69a86fe5
Mukhamed Talaspaev <7636muha@gmail.com>
2019-12-26 02:16:59 +0300

    feat:add local server

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,6 +5,30 @@ import InstaService from '../services/instaService';
 export default class Posts extends Component {
 
     InstaService = new InstaService();
+    state = {
+        posts: {},
+        error: false
+    }
+    componentDidMount() {
+        this.updatePosts();
+    }
+    updatePosts() {
+        this.InstaService.getAllPosts()
+            .then(this.onPostsLoaded)
+            .catch(this.onError);
+    }
+
+    onPostsLoaded = (posts) => {
+        this.setState({
+            posts,
+            error: false
+        })
+    }
+    onError = () => {
+        this.setState({
+            error: true
+        })
+    }
 
     render() {
         return (
